0c42abc7222873f65454f71e7d3d0d0a4f2d2467
kirill22111333 <kivahnin@gmail.com>
2023-05-10 21:51:00 +0300

    feat: install redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
 import Routing from 'pages'
 import './App.css'
 import withRouter from './hooks'
+import { Provider } from 'react-redux'
+import { appStore } from './appStore'
 
 function App() {
-  return (
-    <>
-      <Routing />
-    </>
-  )
+    return (
+        <>
+            <Provider store={appStore}>
+                <Routing />
+            </Provider>
+        </>
+    )
 }
 
 export default withRouter(App)
